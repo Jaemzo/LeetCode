0aebdbfd0c70cbb4a8018c0d1f3615e513337ea4
James Clark <james.clark.classroom@gmail.com>
2025-04-16 09:24:05 -0400

    a

--- least_commong_pre.py
+++ least_commong_pre.py
@@ -1,8 +1,25 @@
 class Solution(object):
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        i = ["flower", "flow", "flight"]
-        for i in strs:
-            if i[0] == i[1]:
-                print(i[0] + i[1])
-            else:
-                print("Error")
+        common_pre = ""
+        word1 = strs[0]
+        for idx in range(len(word1)):
+            # helps pick out the character(letter) we are looking at
+            for word_idx in range(1, len(strs)):
+                if len(strs[word_idx]) <= idx:
+                    return common_pre
+                # helps pick out the word we are looking at
+                if strs[word_idx][idx] != word1[idx]:
+                    # makes sure charcters match, if they dont, return common prefix
+                    return common_pre
+            common_pre += word1[idx]
+        return common_pre
+
+
+def main():
+    strs = ["flower", "flow", "flight"]
+    sol = Solution()
+    print(sol.longestCommonPrefix(strs))
+
+
+if __name__ == "__main__":
+    main()
